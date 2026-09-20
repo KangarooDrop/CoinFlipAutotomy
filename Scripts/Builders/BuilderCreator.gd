dd73b65999d38679e41b9b6fac958f5f8cf7a9e7
KangarooDrop <kevinmasse@comcast.net>
2026-09-19 23:59:35 -0400

    Minor BuilderCreator bug fixes

--- Scripts/Builders/BuilderCreator.gd
+++ Scripts/Builders/BuilderCreator.gd
@@ -177,8 +177,8 @@ func onCreatePressed() -> void:
 	for i in range(oldDBLines.size()):
 		var line : String = oldDBLines[i]
 		if line.contains("(" + baseClassName + ")"):
+			line = line.substr(1)		#Removes leading # from line
 			dbCheck = line.left(line.find("(" + baseClassName + ")"))
-			dbCheck = dbCheck.substr(1)		#Removes leading # from line
 			baseModelDBAddLine = line
 		if not dbCheck.is_empty() and line.contains(dbCheck):
 			lastDBIndex = i
